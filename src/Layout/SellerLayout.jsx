73b05ffbb9e07a13f46c9137a036e91e070707ee
nuaimhasan <nuaim.emanager@gmail.com>
2024-04-01 22:34:20 +0600

    seller sidebar add

--- src/Layout/SellerLayout.jsx
+++ src/Layout/SellerLayout.jsx
@@ -1,3 +1,33 @@
+import { useEffect, useState } from "react";
+import SellerSidebar from "../components/SellerComponents/SellerSidebar/SellerSidebar";
+import AdminHeader from "../components/AdminComponents/AdminHeader/AdminHeader";
+import { Outlet } from "react-router-dom";
+
 export default function SellerLayout() {
-  return <div>SellerLayout</div>;
+  const [sidebar, setSidebar] = useState(false);
+  useEffect(() => {
+    window.addEventListener("click", (e) => {
+      if (
+        (!e.target.closest(".admin_sidebar") &&
+          !e.target.closest(".admin_sidebar_btn")) ||
+        e.target.closest(".admin_siderbar ul li a")
+      ) {
+        setSidebar(false);
+      }
+    });
+  }, []);
+
+  return (
+    <section className="flex">
+      <aside className={`admin_sidebar ${sidebar && "admin_sidebar_show"}`}>
+        <SellerSidebar />
+      </aside>
+      <div className="admin_content">
+        <AdminHeader setSidebar={setSidebar} />
+        <main className="sm:p-5 py-5">
+          <Outlet />
+        </main>
+      </div>
+    </section>
+  );
 }
---
+++ src/components/SellerComponents/SellerSidebar/SellerSidebar.jsx
@@ -0,0 +1,223 @@
+import { Link } from "react-router-dom";
+import {
+  MdOutlineCategory,
+  MdOutlineDashboard,
+  MdMonitor,
+  MdOutlineSettings,
+} from "react-icons/md";
+import { SiBrandfolder } from "react-icons/si";
+import { RiAdminFill } from "react-icons/ri";
+import { BsCart4 } from "react-icons/bs";
+import { FaUsers } from "react-icons/fa";
+import { BiSolidShoppingBags } from "react-icons/bi";
+import SidebarItems from "./SidebarItems";
+import { useGetMainLogoQuery } from "../../../Redux/logo/logoApi";
+import { VscPreview } from "react-icons/vsc";
+import { IoMdSettings } from "react-icons/io";
+import { MdFlashOn } from "react-icons/md";
+import { CiSearch } from "react-icons/ci";
+
+import { useGetBusinessInfoQuery } from "../../../Redux/businessInfoApi/businessInfoApi";
+
+const adminSidebarItems = [
+  {
+    icon: <MdOutlineDashboard />,
+    title: "Dashbaord",
+    path: "/admin/dashboard",
+  },
+  {
+    icon: <MdOutlineCategory />,
+    title: "Category",
+    subMenu: [
+      {
+        title: "Categories",
+        path: "/admin/category/categories",
+      },
+      {
+        title: "Sub Categories",
+        path: "/admin/category/sub-categories",
+      },
+      {
+        title: "Sub SubCategories",
+        path: "/admin/category/sub-sub-categories",
+      },
+    ],
+  },
+  {
+    icon: <SiBrandfolder />,
+    title: "Brand",
+    path: "/admin/brands",
+  },
+  {
+    icon: <BsCart4 />,
+    title: "Product",
+    subMenu: [
+      {
+        title: "Add New Product",
+        path: "/admin/product/add-product",
+      },
+      {
+        title: "All Products",
+        path: "/admin/product/all-products",
+      },
+    ],
+  },
+  {
+    icon: <BiSolidShoppingBags />,
+    title: "Order",
+    subMenu: [
+      {
+        title: "All Orders",
+        path: "/admin/order/all-orders",
+      },
+    ],
+  },
+  {
+    icon: <MdFlashOn />,
+    title: "Flash Deal",
+    path: "/admin/flash-deal",
+  },
+  {
+    icon: <VscPreview />,
+    title: "Review",
+    path: "/admin/reviews",
+  },
+  {
+    icon: <FaUsers />,
+    title: "Customer",
+    subMenu: [
+      {
+        title: "All Customers",
+        path: "/admin/customer/all-customers",
+      },
+    ],
+  },
+  {
+    icon: <RiAdminFill />,
+    title: "Administrator",
+    subMenu: [
+      {
+        title: "All Administrator",
+        path: "/admin/administrator/all-administrator",
+      },
+    ],
+  },
+  {
+    icon: <MdOutlineSettings />,
+    title: "Banner",
+    subMenu: [
+      {
+        title: "Main Banner",
+        path: "/admin/ecommerce-setting/banner",
+      },
+      {
+        title: "Top Campaign Banner",
+        path: "/admin/ecommerce-setting/top-campaign-banner",
+      },
+      {
+        title: "Campaign Banner",
+        path: "/admin/ecommerce-setting/campaign-banner",
+      },
+    ],
+  },
+  {
+    icon: <MdOutlineSettings />,
+    title: "E-commerce Setting",
+    subMenu: [
+      {
+        title: "Coupon",
+        path: "/admin/ecommerce-setting/coupons",
+      },
+      {
+        title: "Shipping Configuration",
+        path: "/admin/ecommerce-setting/shipping-configuration",
+      },
+    ],
+  },
+  {
+    icon: <IoMdSettings />,
+    title: "General Setting",
+    slug: "general-setting",
+    subMenu: [
+      {
+        title: "Profile",
+        path: "/admin/general-setting/profile",
+      },
+      {
+        title: "Business Info",
+        path: "/admin/general-setting/business-info",
+      },
+      {
+        title: "Themes",
+        path: "/admin/general-setting/themes",
+      },
+    ],
+  },
+  {
+    icon: <MdMonitor />,
+    title: "Front-End Setting",
+    subMenu: [
+      {
+        title: "Logo",
+        path: "/admin/front-end/logo",
+      },
+      {
+        title: "Favicon",
+        path: "/admin/front-end/favicon",
+      },
+      {
+        title: "About Us",
+        path: "/admin/front-end/about-us",
+      },
+      {
+        title: "Contact Us",
+        path: "/admin/front-end/contact-us",
+      },
+    ],
+  },
+  {
+    icon: <CiSearch className="text-lg" />,
+    title: "SEO Setting",
+    path: "/admin/seo-setting",
+  },
+];
+
+export default function SellerSidebar() {
+  const { data } = useGetMainLogoQuery();
+  const { data: businessInfo } = useGetBusinessInfoQuery();
+
+  return (
+    <div className="h-full flex flex-col justify-between">
+      <div>
+        <Link to="/admin/dashboard" className="block border-b py-4">
+          <img
+            src={
+              data?.data[0]?.logo === null
+                ? "/images/logo/logo.png"
+                : `${import.meta.env.VITE_BACKEND_URL}/logo/${
+                    data?.data[0]?.logo
+                  }`
+            }
+            alt=""
+            className="w-28 mx-auto h-16"
+          />
+        </Link>
+
+        <nav className="admin_siderbar">
+          <ul>
+            {adminSidebarItems?.map((item, i) => (
+              <SidebarItems key={i} item={item} />
+            ))}
+          </ul>
+        </nav>
+      </div>
+
+      <div className="bg-[#445360] p-2 flex justify-between items-center font-light">
+        <p>Visit Front-End</p>
+        <Link to="/" target="_blank" className="text-primary hover:underline">
+          {businessInfo?.data[0]?.companyName}
+        </Link>
+      </div>
+    </div>
+  );
+}
