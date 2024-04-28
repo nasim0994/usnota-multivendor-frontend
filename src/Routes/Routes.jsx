2ca1d4dedda3a4be740d0ce8134e199963d5db4e
nuaimhasan <nuaim.emanager@gmail.com>
2024-04-28 17:18:47 +0600

    update editcategory

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -32,7 +32,7 @@ import Dashboard from "../pages/Admin/Dashboard/Dashboard";
 //---------------Category
 import AddCategory from "../pages/Admin/Category/Categories/AddCategory";
 import AllCategories from "../pages/Admin/Category/Categories/AllCategories";
-import EditCategory from "../pages/Admin/Category/Categories/EditCategory";
+import EditCategory from "../pages/Admin/Category/Categories/Editcategory";
 
 import AddSubCategory from "../pages/Admin/Category/SubCategories/AddSubCategory";
 import AllSubCategories from "../pages/Admin/Category/SubCategories/AllSubCategories";
